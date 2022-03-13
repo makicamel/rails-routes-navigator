2d69d9a2016a60dd241e1570b8ef5bba15fad867
makicamel <unright@gmail.com>
2022-03-13 16:06:43 +0900

    Rename command

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('rails-routes-navigator.helloWorld', () => {
+  let disposable = vscode.commands.registerCommand('rails-routes-navigator.railsRoutesNavigate', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Rails Routes Navigator!');
+    vscode.window.showInformationMessage('Hello World! from Rails Routes Navigator!');
   });
 
   context.subscriptions.push(disposable);
