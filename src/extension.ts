25df9ea1a1fc07b8f548078a97917ff405958a1f
makicamel <unright@gmail.com>
2022-03-16 07:28:34 +0900

    Use input instead of showInputBox

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,18 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
+  let currentPanel: vscode.WebviewPanel | undefined = undefined;
   let disposable = vscode.commands.registerCommand('rails-routes-navigator.railsRoutesNavigate', () => {
-    vscode.window.showInputBox({
-      prompt: 'Input Rails Routes',
-      validateInput: (input: string): string | undefined =>
-        (!input) ? 'Input some chars for Rails routes' : undefined
-    }).then(
-      inputString => {
-        if (!inputString) { return; };
-
-        const panel = vscode.window.createWebviewPanel(
-          'railsRoutesNavigator',
-          'Rails Routes Navigator',
-          vscode.ViewColumn.Two,
-          {}
-        );
-        panel.webview.html = getWebviewContent(panel.webview, inputString);
+    currentPanel = vscode.window.createWebviewPanel(
+      'railsRoutesNavigator',
+      'Rails Routes Navigator',
+      vscode.ViewColumn.Two,
+      {
+        enableScripts: true,
       }
     );
+
+    currentPanel.webview.html = getWebviewContent(currentPanel.webview);
   });
 
   context.subscriptions.push(disposable);
@@ -27,16 +20,27 @@ export function activate(context: vscode.ExtensionContext) {
 
 export function deactivate() { }
 
-function getWebviewContent(webview: vscode.Webview, inputString: string) {
+function getWebviewContent(webview: vscode.Webview) {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https:;">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' ${webview.cspSource} https:; script-src 'nonce-11032b2d27d2' ">
   <title>Rails Routes Navigator</title>
 </head>
 <body>
   <h1>Rails Routes Navigator</h1>
-  ${inputString}
+  <input type="search" id="search" placeholder="Input some chars for Rails routes" />
+  <div id="input-chars"></div>
+
+  <script nonce="11032b2d27d2">
+    let search = document.getElementById('search');
+    const chars = document.getElementById('input-chars');
+
+    search.addEventListener('keyup', () => {
+      chars.innerHTML = search.value;
+    });
+  </script>
+
 </body>
 </html>`;
 }
